ef4e7344d35b8bb59e3b56029e91b6ef743cd493
A'zamov Samandar <admin@jscorp.uz>
2024-12-25 18:49:09 +0500

    ViewSet'ni o'zgartirdi: ReadOnlyModelViewSet -> ModelViewSet; moderator ruxsatlarini qo'shdi.

--- core/apps/news/views/post.py
+++ core/apps/news/views/post.py
@@ -3,14 +3,15 @@ from typing import Any
 from django_core.mixins import BaseViewSetMixin
 from drf_spectacular.utils import extend_schema
 from rest_framework.permissions import AllowAny
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet
+from core.apps.accounts.permissions import IsModeratorPermission
 
 from ..models import PostModel
 from ..serializers.post import CreatePostSerializer, ListPostSerializer, RetrievePostSerializer
 
 
 @extend_schema(tags=["post"])
-class PostView(BaseViewSetMixin, ReadOnlyModelViewSet):
+class PostView(BaseViewSetMixin, ModelViewSet):
 
     def get_queryset(self):
         return PostModel.objects.order_by("-created_at")
@@ -29,6 +30,8 @@ class PostView(BaseViewSetMixin, ReadOnlyModelViewSet):
     def get_permissions(self) -> Any:
         perms = []
         match self.action:
+            case "create" | "update" | "partial_update" | "destroy":
+                perms.extend([IsModeratorPermission])
             case _:
                 perms.extend([AllowAny])
         self.permission_classes = perms
