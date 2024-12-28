64ed8c283bd3aa7f5a65daad69dd2667584e7bee
A'zamov Samandar <admin@jscorp.uz>
2024-12-28 16:13:14 +0500

    fix: kurslarni tekshirish va fayl serializerini yangilash

--- core/apps/shared/serializers/file.py
+++ core/apps/shared/serializers/file.py
@@ -6,20 +6,27 @@ class FileSerializer(serializers.Serializer):
     url = serializers.SerializerMethodField()
     size = serializers.SerializerMethodField()
     extension = serializers.SerializerMethodField()
+    name = serializers.SerializerMethodField()
 
-    def get_url(self, obj):
+    def get_name(self, obj) -> str:
+        try:
+            return obj.file.name.split("/")[-1]
+        except AttributeError:
+            return None
+
+    def get_url(self, obj) -> str:
         try:
             return self.context["request"].build_absolute_uri(obj.url)
         except Exception:
             return None
 
-    def get_size(self, obj):
+    def get_size(self, obj) -> str:
         try:
             return FileUtils.get_file_size(obj.size)
         except Exception:
             return None
 
-    def get_extension(self, obj):
+    def get_extension(self, obj) -> str:
         try:
             return FileUtils.get_extension(obj.name)
         except Exception:
