cc4b05ce75ed09b09e104d6e85b0165d34fe3c40
JAvohir <uchihajavohir0609@gmail.com>
2025-01-24 11:40:22 +0500

    +user view and serializer

---
+++ core/apps/accounts/views/user.py
@@ -0,0 +1,38 @@
+from django.utils.translation import gettext_lazy as _
+from django_core.mixins import BaseViewSetMixin
+from drf_spectacular.utils import  extend_schema
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from ..serializers import (
+    UserCreateSerializer,
+    UserListSerializer,
+    UserRetrieveSerializer,
+    UserUpdateSerializer,
+)
+
+from drf_spectacular.utils import extend_schema
+from django.contrib.auth import get_user_model
+from django_core.mixins import BaseViewSetMixin
+
+from rest_framework.viewsets import ModelViewSet
+from ..permissions import AdminPermission
+
+
+@extend_schema(tags=["user"])
+class UserView(BaseViewSetMixin, ModelViewSet):
+    queryset = get_user_model().objects.all()
+    permission_classes = [IsAuthenticated,AdminPermission]
+
+    def get_serializer_class(self):
+        match self.action:
+            case "create":
+                return UserCreateSerializer
+            case "update":
+                return UserUpdateSerializer
+            case "list":
+                return UserListSerializer
+            case "retreive":
+                return UserRetrieveSerializer
+            case _:
+                return UserListSerializer
+        return super().get_serializer_class()
